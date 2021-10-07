82b4936689c7174be432d56208b2ee70a0433004
Selim <selimchemutai19@gmail.com>
2021-10-07 19:52:19 +0300

    Update SettingsFragment.kt

--- app/src/main/java/com/pamela/globochat/SettingsFragment.kt
+++ app/src/main/java/com/pamela/globochat/SettingsFragment.kt
@@ -1,18 +1,12 @@
 package com.pamela.globochat
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayoutStates
-import androidx.navigation.fragment.NavHostFragment
 import androidx.preference.PreferenceFragmentCompat
 
-
+//Class PreferenceFragmentCompat() used to Implement settings in android
 class SettingsFragment :PreferenceFragmentCompat() {
 
-override fun onCreatePrefrences(savedInstancesStates: Bundle?, rootkey:String){
+override fun onCreatePreferences(savedInstancesStates: Bundle?, rootkey:String){
 
     setPreferencesFromResource(R.xml.settings, rootkey)
 }
