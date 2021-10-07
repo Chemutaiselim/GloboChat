7b0ba34256e2d8a099c2ac722ef8e2bbda0baa13
Selim <selimchemutai19@gmail.com>
2021-10-07 19:11:39 +0300

    Update SettingsFragment.kt

--- app/src/main/java/com/pamela/globochat/SettingsFragment.kt
+++ app/src/main/java/com/pamela/globochat/SettingsFragment.kt
@@ -5,24 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayoutStates
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment :PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+override fun onCreatePrefrences(savedInstancesStates: Bundle?, rootkey:String){
 
-        view.setOnClickListener {
-            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-            val navController = navHostFragment.navController
-            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
-            navController.navigate(action)
-        }
+    setPreferencesFromResource(R.xml.settings, rootkey)
+}
 
-        return view
-    }
 }
