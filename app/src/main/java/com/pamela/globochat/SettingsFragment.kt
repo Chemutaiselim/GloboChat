6ab521847a5b91705fa11811384e72f9476e7950
Selim <selimchemutai19@gmail.com>
2021-10-08 14:44:45 +0300

    Update SettingsFragment.kt

--- app/src/main/java/com/pamela/globochat/SettingsFragment.kt
+++ app/src/main/java/com/pamela/globochat/SettingsFragment.kt
@@ -1,7 +1,12 @@
 package com.pamela.globochat
 
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 //Class PreferenceFragmentCompat() used to Implement settings in android
 class SettingsFragment :PreferenceFragmentCompat() {
@@ -9,6 +14,29 @@ class SettingsFragment :PreferenceFragmentCompat() {
 override fun onCreatePreferences(savedInstancesStates: Bundle?, rootkey:String){
 
     setPreferencesFromResource(R.xml.settings, rootkey)
-}
+    val accSettingsPref = findPreference<Preference>(getString(R.string.key_account_settings))
 
+    accSettingsPref?.setOnPreferenceClickListener {
+
+        val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        val navController = navHostFragment.navController
+        val action = SettingsFragmentDirections.actionSettingsToAccSettings()
+        navController.navigate(action)
+        true
+    }
+    // Read Preference values in a Fragment
+    // Step 1: Get reference to the SharedPreferences (XML File)
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    // Step 2: Get the 'value' using the 'key'
+    val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time), "")
+    Log.i("SettingsFragment", "Auto Reply Time: $autoReplyTime")
+
+    val autoDownload = sharedPreferences.getBoolean(getString(R.string.key_auto_download), false)
+    Log.i("SettingsFragment", "Auto Download: $autoDownload")
+
+    val statusPref = findPreference<EditTextPreference>(getString(R.string.key_status))
+    statusPref?.setOnPreferenceChangeListener { preference, newValue ->
+
+
+    }
 }
