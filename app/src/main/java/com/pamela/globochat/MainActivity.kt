e022612937f15de726e57519a001b3e6c1e7a21d
Selim <selimchemutai19@gmail.com>
2021-10-11 20:51:35 +0300

    Update MainActivity.kt

--- app/src/main/java/com/pamela/globochat/MainActivity.kt
+++ app/src/main/java/com/pamela/globochat/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.pamela.globochat
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.preference.PreferenceManager
 import com.pamela.globochat.R
 
 
@@ -32,5 +35,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    // Called only 'after' the Preference value has changed
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+
+        if (key == getString(R.string.key_status)) {
+            val newStatus = sharedPreferences?.getString(key, "")
+            Toast.makeText(this, "New Status: $newStatus", Toast.LENGTH_SHORT).show()
+        }
+
+        if (key == getString(R.string.key_auto_reply)) {
+
+            val autoReply = sharedPreferences?.getBoolean(key, false)
+            if (autoReply!!) {
+                Toast.makeText(this, "Auto Reply: ON", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Auto Reply: OFF", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
     }
 }
